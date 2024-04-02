882dc30182dd0d5880a310c30a9b722a4513db1f
Arafat <arafatmannan2019@gmail.com>
2024-04-02 21:02:56 +0600

    lib service developing

--- src/lib/lib.service.ts
+++ src/lib/lib.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@nestjs/common';
 import Argon2 from 'argon2';
 import { HttpStatus } from '@nestjs/common/enums'
 import { HttpException } from '@nestjs/common/exceptions'
+import cloudinary from 'cloudinary'
+import { ConfigService } from "@nestjs/config/dist/config.service";
+import axios from "axios";
+import sharp from "sharp";
+
 
 @Injectable()
 export class LibService {
-    constructor() {
+    constructor(private config: ConfigService,) {
+
+
     }
 
-    // argon start
+    // _______________argon start_______________
     //******* get hased passweord *******
     async getHashed(plainStr: string): Promise<string> {
         try {
@@ -20,13 +27,67 @@ export class LibService {
     }
 
     //******* Veryfy passweord *******
-    async VerifyPassword(HasedPassword, PlainPassword: string) {
+    async VerifyPassword(HashedPassword, PlainPassword: string) {
         try {
-            return await Argon2.verify(HasedPassword, PlainPassword)
+            return await Argon2.verify(HashedPassword, PlainPassword)
         } catch (error) {
             console.log(error);
             throw new HttpException('Something Went Wrong', HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-    // argon End
+    // _______________argon End_______________
+    // _______________Cld Start_______________
+    cldInitiate() {
+        cloudinary.v2.config({
+            cloud_name: this.config.get('CLOUDINARY_NAME'),
+            api_key: this.config.get('CLOUDINARY_API_KEY'),
+            api_secret: this.config.get('CLOUDINARY_API_SECRET')
+        });
+    }
+
+    async cldUpload(url: string) {
+        await this.cldInitiate();
+        try {
+            const Cls = await cloudinary.v2.uploader.upload(url);
+            const data = {
+                url: Cls.url,
+                width: Cls.width,
+                height: Cls.height
+            };
+            return data;
+        } catch (error) {
+            console.log(error);
+            throw new HttpException('Something Went Wrong', HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    // _______________Cld End_______________
+    // _______________Sherp Start_______________
+    async uploadWithSharp(url: string) {
+        try {
+            const imgBuffer = await axios.get(url, { responseType: 'arraybuffer' });
+            const imageBuffer = Buffer.from(imgBuffer.data, 'binary');
+            const data = await sharp(imageBuffer).png({ quality: 20 }).toBuffer();
+            const base64String = data.toString('base64');
+            const cld = await this.cldUpload(`data:image/png;base64,${base64String}`);
+            if (cld && cld.url) {
+                return {
+                    isSuccess: true,
+                    url: cld.url
+                };
+            } else {
+                return {
+                    isSuccess: false,
+                    url: 'NOT_FOUND'
+                };
+            }
+        } catch (error) {
+            console.log(error);
+            return {
+                isSuccess: false,
+                url: 'FAILED'
+            };
+        }
+    }
+
+    // _______________Sherp End_______________
 }
