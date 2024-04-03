1893f251064a0b13ca42268cb70efcb4ecacf852
arafat2020 <67102280+arafat2020@users.noreply.github.com>
2024-04-03 07:36:55 +0000

    initiate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Light Chat Server')
+    .setDescription('A Backend Massaging Server')
+    .setVersion('1.0')
+    .addTag('Light Chat')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   app.use(express.static('/client'))
   await app.listen(3000);
 }
